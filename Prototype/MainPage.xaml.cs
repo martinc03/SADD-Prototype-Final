211417e035a16cd362e953af58cb7d10265a6bea
martin <martin.conreras@edu.sait.ca>
2023-12-08 05:27:34 -0700

    push it

---
+++ Prototype/MainPage.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace Prototype
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ Prototype/obj/Debug/net8.0-android/android/src/crc6441c42df41a048f9a/MainActivity.java
@@ -0,0 +1,48 @@
+package crc6441c42df41a048f9a;
+
+
+public class MainActivity
+	extends crc6488302ad6e9e4df1a.MauiAppCompatActivity
+	implements
+		mono.android.IGCUserPeer
+{
+/** @hide */
+	public static final String __md_methods;
+	static {
+		__md_methods = 
+			"";
+		mono.android.Runtime.register ("Prototype.MainActivity, Prototype", MainActivity.class, __md_methods);
+	}
+
+
+	public MainActivity ()
+	{
+		super ();
+		if (getClass () == MainActivity.class) {
+			mono.android.TypeManager.Activate ("Prototype.MainActivity, Prototype", "", this, new java.lang.Object[] {  });
+		}
+	}
+
+
+	public MainActivity (int p0)
+	{
+		super (p0);
+		if (getClass () == MainActivity.class) {
+			mono.android.TypeManager.Activate ("Prototype.MainActivity, Prototype", "System.Int32, System.Private.CoreLib", this, new java.lang.Object[] { p0 });
+		}
+	}
+
+	private java.util.ArrayList refList;
+	public void monodroidAddReference (java.lang.Object obj)
+	{
+		if (refList == null)
+			refList = new java.util.ArrayList ();
+		refList.add (obj);
+	}
+
+	public void monodroidClearReferences ()
+	{
+		if (refList != null)
+			refList.clear ();
+	}
+}
